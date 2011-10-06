b32525590a4fb31f0ada1ff78c692784d7cc7619
Chinh Tran <chinht@codeaurora.org>
2011-10-05 17:16:19 -0700

    external/libxml2: Define  LIBXML_TREE_ENABLED in the Android make file
    The LIBXML_TREE_ENABLED is required to insert element into xml file.
    
    Change-Id: I8b815115c2546fc2766196fa71c44c607e234892

--- Android.mk
+++ Android.mk
@@ -59,7 +59,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_C_INCLUDES += $(common_C_INCLUDES) external/icu4c/common
 LOCAL_SHARED_LIBRARIES += $(common_SHARED_LIBRARIES)
-LOCAL_CFLAGS += -fvisibility=hidden
+LOCAL_CFLAGS += -fvisibility=hidden -DLIBXML_TREE_ENABLED
 
 LOCAL_MODULE:= libxml2
 
